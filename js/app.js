6944a528bfbc0a5fa3c541e4347e0d258b94f7b7
yousef-97 <alshenyousef@gmail.com>
2020-02-17 22:24:45 +0200

    Aboutme

--- js/app.js
+++ js/app.js
@@ -1,52 +1,71 @@
-// alert('hi');
 'use strict';
 var q1 = prompt('do you like vedio games?').toUpperCase();
 if (q1 === 'yes' || q1 ==='YES' || q1 === 'y' || q1 === 'Y') {
+//   console.log('you are my homie');
   alert('you are my homie');
 }
 else if (q1 === 'no' || q1 === 'NO' || q1 === 'n' || q1 === 'N') {
+//   console.log('im sad:(');
   alert('im sad :(');
 }
-else { alert('PLZ choose (yes , no , y , n)'); }
+else {
+  // console.log('PLZ choose (yes , no , y , n)');
+  alert('PLZ choose (yes , no , y , n)'); }
 
 
 var q2 = prompt('did you finish your lab yesterday?').toUpperCase();
 if (q2 === 'yes' || q2 ==='YES' || q2 === 'y' || q2 === 'Y') {
+//   console.log('good boii');
   alert('good boii');
 }
 else if (q2 === 'no' || q2 === 'NO' || q2 === 'n' || q2 === 'N') {
+//   console.log('WHY BRO?');
   alert('WHY BRO?');
 }
-else { alert('PLZ choose (yes , no , y , n)'); }
+else {
+  // console.log('PLZ choose (yes , no , y , n)');
+  alert('PLZ choose (yes , no , y , n)'); }
 
 var q3 = prompt('can you fly ?').toUpperCase();
 if (q3 === 'yes' || q3 ==='YES' || q3 === 'y' || q3 === 'Y') {
+//   console.log('YES you can BIRD LOL');
   alert('YES you can BIRD LOL');
 }
 else if (q3 === 'no' || q3 === 'NO' || q3 === 'n' || q3 === 'N') {
+//   console.log('unfortunately ');
   alert('unfortunately ');
 }
-else { alert('PLZ choose (yes , no , y , n)'); }
+else {
+  // console.log('PLZ choose (yes , no , y , n)');
+  alert('PLZ choose (yes , no , y , n)'); }
 
 
 var q4 = prompt('do you like cats?').toUpperCase();
 if (q4 === 'yes' || q4 ==='YES' || q4 === 'y' || q4 === 'Y') {
+//   console.log('MIOW');
   alert('MIOW');
 }
 else if (q4 === 'no' || q4 === 'NO' || q4 === 'n' || q4 === 'N') {
+//   console.log('why? its fluffy and cute');
   alert('why? its fluffy and cute');
 }
-else { alert('PLZ choose (yes , no , y , n)'); }
+else {
+  // console.log('PLZ choose (yes , no , y , n)');
+  alert('PLZ choose (yes , no , y , n)'); }
 
 
 var q5 = prompt('can i leave the class?').toUpperCase();
 if (q5 === 'yes' || q5 ==='YES' || q5 === 'y' || q5 === 'Y') {
+//   console.log('its 6pm');
   alert('its 6pm ');
 }
 else if (q5 === 'no' || q5 === 'NO' || q5 === 'n' || q5 === 'N') {
+//   console.log('too early to leave');
   alert('too early to leave');
 }
-else { alert('PLZ choose (yes , no , y , n)'); }
+else {
+  // console.log('PLZ choose (yes , no , y , n)');
+  alert('PLZ choose (yes , no , y , n)'); }
 
 var userName = prompt('whats your name?');
 alert('most welcome ' + userName);
