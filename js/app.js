1f8651c7aedb642537300c1a9f535f9785ed5682
yousef-97 <alshenyousef@gmail.com>
2020-02-18 22:13:32 +0200

    lab03

--- js/app.js
+++ js/app.js
@@ -1,67 +1,75 @@
 'use strict';
-var q1 = prompt('do you like vedio games?').toUpperCase();
-if (q1 ==='YES' ||q1 === 'Y') {
-//   console.log('you are my homie');
-  alert('you are my homie');
+var z=0;
+//1st Q
+
+var gamesLike = prompt('do i like vedio games?').toUpperCase();
+if (gamesLike ==='YES' ||gamesLike === 'Y') {
+//   console.log('yes i\'m');
+  alert('correct');z++;
 }
-else if (q1 === 'NO' ||q1 === 'N') {
-//   console.log('im sad:(');
-  alert('im sad :(');
+else if (gamesLike === 'NO' ||gamesLike === 'N') {
+//   console.log('wrong answer');
+  alert('wrong answer');
 }
 else {
   // console.log('PLZ choose (yes , no , y , n)');
   alert('PLZ choose (yes , no , y , n)'); }
 
+//2nd Q
 
-var q2 = prompt('did you finish your lab yesterday?').toUpperCase();
-if (q2 ==='YES' || q2 === 'Y') {
+var swimng = prompt('can i swim?').toUpperCase();
+if (swimng ==='YES' || swimng === 'Y') {
 //   console.log('good boii');
-  alert('good boii');
+  alert('i can not');
 }
-else if (q2 === 'NO'|| q2 === 'N') {
-//   console.log('WHY BRO?');
-  alert('WHY BRO?');
+else if (swimng === 'NO'|| swimng === 'N') {
+//   console.log('yes im bad');
+  alert('yes im bad');z++;
 }
 else {
   // console.log('PLZ choose (yes , no , y , n)');
   alert('PLZ choose (yes , no , y , n)'); }
 
-var q3 = prompt('can you fly ?').toUpperCase();
-if (q3 ==='YES'|| q3 === 'Y') {
-//   console.log('YES you can BIRD LOL');
-  alert('YES you can BIRD LOL');
+//3rd Q
+
+var flying = prompt('can i fly ?').toUpperCase();
+if (flying ==='YES'|| flying === 'Y') {
+//   console.log('im not a BIRD LOL');
+  alert('im not a BIRD LOL');
 }
-else if (q3 === 'NO' ||q3 === 'N') {
+else if (flying === 'NO' ||flying === 'N') {
 //   console.log('unfortunately ');
-  alert('unfortunately ');
+  alert('unfortunately ');z++;
 }
 else {
   // console.log('PLZ choose (yes , no , y , n)');
   alert('PLZ choose (yes , no , y , n)'); }
 
+//4th Q
 
-var q4 = prompt('do you like cats?').toUpperCase();
-if (q4 ==='YES' || q4 === 'Y') {
+var cat = prompt('do i like cats?').toUpperCase();
+if (cat ==='YES' || cat === 'Y') {
 //   console.log('MIOW');
-  alert('MIOW');
+  alert('MIOW');z++;
 }
-else if (q4 === 'NO' || q4 === 'N') {
-//   console.log('why? its fluffy and cute');
-  alert('why? its fluffy and cute');
+else if (cat === 'NO' || cat === 'N') {
+//   console.log('cats are fluffy and cute');
+  alert('cats are fluffy and cute');
 }
 else {
   // console.log('PLZ choose (yes , no , y , n)');
   alert('PLZ choose (yes , no , y , n)'); }
 
+//5th Q
 
-var q5 = prompt('can i leave the class?').toUpperCase();
-if (q5 ==='YES' || q5 === 'Y') {
-//   console.log('its 6pm');
-  alert('its 6pm ');
+var mansaf = prompt('do i love mansaf?').toUpperCase();
+if (mansaf ==='YES' || mansaf === 'Y') {
+//   console.log('yami');
+  alert('yami');z++;
 }
-else if (q5 === 'NO' || q5 === 'N') {
-//   console.log('too early to leave');
-  alert('too early to leave');
+else if (mansaf === 'NO' || mansaf === 'N') {
+//   console.log('GG');
+  alert('GG');
 }
 else {
   // console.log('PLZ choose (yes , no , y , n)');
@@ -70,3 +78,37 @@ else {
 var userName = prompt('whats your name?');
 alert('most welcome ' + userName);
 alert('lets go ' + userName);
+
+
+//6th Q
+
+var luckyNum = 12;
+var guessNum = prompt('guess my lucky number','0-25');
+// while (guessNum < 0 || guessNum>100){guessNum = prompt('guess my luky number','0-100');}
+
+for(var i=1 ; i<=3 ; i++){
+  if(Number(guessNum)===luckyNum){alert('that\'s correct');z++;break;}
+  // else if(Number(guessNum)>lovleyNum && Number(guessNum)<=lovleyNum+20){alert('high but close');}
+  // else if(Number(guessNum)<lovleyNum && Number(guessNum)>=lovleyNum-20){alert('low but close');}
+  else if(Number(guessNum)>luckyNum){alert('too high');}
+  else if(Number(guessNum)<luckyNum){alert('too low');}
+  guessNum = prompt('guess again','0-25');
+  if (i===3){alert('answer is '+ luckyNum);}
+}
+
+//7th Q
+
+var topCar=['bmw','mitsubishi','mercedes','mini cooper','chevrolet','ferrari','purusha'];
+var gameGuess = prompt('guess one of my lovely cars','you have 6 attempt').toLowerCase();
+
+for( var counter=1;counter<=5;counter++){
+
+  for(var otherCounter=0 ; otherCounter <= topCar.length-1;otherCounter++ ){
+    if(topCar[otherCounter]===gameGuess){alert('NICE Guessing');z++;alert('answer is all of these '+topCar);break;}
+  }
+  if (topCar[otherCounter]===gameGuess){break;}
+  gameGuess = prompt('guess again');
+  if (counter===5){alert('answer is all of these '+topCar);}
+
+}
+alert('Your score '+z+' of 7');
